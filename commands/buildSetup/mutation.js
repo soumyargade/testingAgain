b92971887a30c93882f6a53145f641bf7d6be969
Tanner Neff <wneff@ncsu.edu>
2022-04-04 12:00:45 -0400

    updated mutation operations to remove counts

--- commands/buildSetup/mutation.js
+++ commands/buildSetup/mutation.js
@@ -1,16 +1,15 @@
 const {MutationOperations} = require('../../lib/exec/mutationOperations');
 
 class Mutation {
-    constructor(inFile, outFile, count, operation) {
+    constructor(inFile, outFile, operation) {
         this.inFile = inFile;
         this.outFile = outFile;
-        this.count = count;
         this.operation = operation;
     }
 
     async mutate() {
         try {
-            MutationOperations.rewrite(this.inFile, this.outFile, this.count, this.operation);
+            MutationOperations.rewrite(this.inFile, this.outFile, this.operation);
         } catch (e) {
             throw `Unable to perform mutation operations. ${e}`;
         }
--- commands/mutate.js
+++ commands/mutate.js
@@ -1,26 +1,22 @@
 const chalk = require("chalk");
 const {Mutation} = require("./buildSetup/mutation");
 
-exports.command = "mutate [input_file] [output_file] [mutation_count] [mutation_function]";
+exports.command = "mutate [input_file] [output_file] [mutation_function]";
 exports.desc = "Perform mutations on a file";
 exports.builder = (yargs) => {
     yargs.options({});
 }
 
 exports.handler = async (argv) => {
-    let { input_file, output_file, mutation_count, mutation_function } = argv;
+    let { input_file, output_file, mutation_function } = argv;
 
-   
-    if (!mutation_count) {
-        mutation_count = 50;
-    }
     if (!mutation_function) {
         mutation_function = "random";
     }
 
-    let mutation = new Mutation(input_file, output_file, mutation_count, mutation_function);
+    let mutation = new Mutation(input_file, output_file, mutation_function);
 
-    console.log(chalk.green(`File ${input_file} will be mutated ${mutation_count} times and the results placed into file ${output_file}`));
+    console.log(chalk.green(`File ${input_file} will be mutated results placed into file ${output_file}`));
 
     mutation.mutate();
 
