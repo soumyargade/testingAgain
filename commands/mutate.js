9418bb96804c5b4bb25eaf4f5655b6550629d44d
Tanner Neff <wneff@ncsu.edu>
2022-04-06 14:42:25 -0400

    added create directory functionality

--- commands/mutate.js
+++ commands/mutate.js
@@ -5,7 +5,7 @@ exports.command = "mutate [file_glob] [mutation_function]";
 exports.desc = "Perform mutations on a file";
 exports.builder = (yargs) => {
     yargs.options({
-        output: {
+        output_directory: {
             alias: 'o',
             describe: 'output directory',
             default: '.',
@@ -15,17 +15,13 @@ exports.builder = (yargs) => {
 }
 
 exports.handler = async (argv) => {
-    let { file_glob, o, mutation_function} = argv;
+    let { file_glob, output_directory, mutation_function} = argv;
 
     if (!mutation_function) {
         mutation_function = "random";
     }
 
-
-
-    let mutation = new Mutation(file_glob, o, mutation_function);
-
-    // console.log(chalk.green(`File ${file_glob} will be mutated results placed into file ${output_file}`));
+    let mutation = new Mutation(file_glob, output_directory, mutation_function);
 
     mutation.mutate();
 
