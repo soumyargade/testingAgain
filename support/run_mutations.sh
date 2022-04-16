44ade88bbb2cbabd7a9edd918dea72563f7d753a
Edwin Sarver <edwin.l.sarver@gmail.com>
2022-04-16 15:49:55 -0400

    create new script that does all the things

---
+++ support/run_mutations.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+usage() {
+    cat << 'EOF'
+$0 [OPTIONS] [GLOB_0 [GLOB_1 [... GLOB_N]]]
+
+  GLOB_[0-N]        Can be an expanded or unexpanded file-glob, if no GLOB is
+                        provided, the command will run without any mutations
+
+  OPTIONS
+    -u URL          A URL from which to take a screenshot, multiple may be 
+                        specified with separate -u flags
+    -c CMD          The command to run to start the server
+    -o OUTDIR       The directory to put all the output files
+    -p PROJDIR      The root directory of the project being tested
+    -n ITERATIONS   The number of iterations to perform
+EOF
+}
+
+declare -a URLS
+declare -a GLOBS
+declare -i ITERATIONS
+declare CMD
+declare OUTDIR
+declare PROJDIR
+
+# Arg-parsing modified from https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+
+usage_error () { 
+    echo >&2 "$(basename $0):   $1"
+    usage
+    exit 2
+}
+
+assert_argument () { 
+    test "$1" != "$EOL" || usage_error "$2 requires an argument"
+}
+
+take_snapshots () {
+    local SUFFIX="$1"
+    local -a pids
+    for u in "${URLS[@]}"; do
+        # copied and modified from https://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
+        pic_name = "$(echo "$u" | rev | cut -d/ -f1 | rev)"
+
+        node /bakerx/support/index.js screenshot "$u" "${OUTDIR}/${pic_name}${SUFFIX}" & pids+=($!)
+    done
+    # wait for pids modified from https://stackoverflow.com/a/40380837/706796
+    wait "${pids[@]}"
+}
+
+run_step () {
+    # Run original code and collect snapshots to compare against
+    eval "$CMD" &
+    local server_pid=$!
+
+    # Wait until the node server is actually listening, do this by polling `lsof`
+    # until a command with the $server_pid is listening on port 3000. This is hacky, but 
+    # seems to be effective.
+    MAX_RETRIES=50
+    let retries=0
+    while ! lsof -nP -iTCP -sTCP:LISTEN | grep -q "${server_pid}.*3000"; do 
+        test retries <= MAX_RETRIES || break
+        let "retries+=1"
+    done
+
+    take_snapshots "0"
+
+    kill "$server_pid"
+}
+
+coverage_report () {
+    for u in "${URLS[@]}"; do
+        # copied and modified from https://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
+        let filename="$(echo "$u" | rev | cut -d/ -f1 | rev)"
+        let pic_name="$filename.png"
+        let file_count=-1
+        let file_mutations=0
+        for f in "${OUTDIR}/${u}*"; do
+           let "file_count+=1"
+           if cmp -q "${OUTDIR}/$pic_name" "${OUTDIR}/$f"; then
+               cmp "$pic_name" "$f"
+               let "file_mutations+=1"
+           fi
+        done
+
+        printf '\n%s mutation snapshots: %s' "$1" "$file_count"
+        printf '\n%s file mutations found: %s' "$1" "$file_mutations"
+        printf '\nMutation coverage: %.3f' "$((10**3 * $file_mutations/$file_count))e-3"
+        printf '\nServer errors: %.f\n\n' "$(($ITERATIONS - $file_count))"
+    done
+}
+
+if [ "$#" != 0 ]; then
+    EOL=$(echo '\01\03\03\07')
+    set -- "$@" "$EOL"
+    while [ "$1" != "$EOL" ]; do
+        opt="$1"; shift
+        case "$opt" in
+
+            # OPTIONS
+            -u) assert_argument "$1" "$opt"; URLS[${#URLS[@]}]="$!"; shift;;
+            -c) assert_argument "$1" "$opt"; CMD="$1"; shift;;
+            -o) assert_argument "$1" "$opt"; OUTDIR="$1"; shift;;
+            -p) assert_argument "$1" "$opt"; PROJDIR="$1"; shift;;
+            -n) assert_argument "$1" "$opt"; ITERATIONS="$1"; shift;;
+
+            #PROCESSING
+            -|''|[!-]*) set -- "$@" "$opt";;                                          # positional argument, rotate to the end
+            --*=*)      set -- "${opt%%=*}" "${opt#*=}" "$@";;                        # convert '--name=arg' to '--name' 'arg'
+            -[!-]?*)    set -- $(echo "${opt#-}" | sed 's/\(.\)/ -\1/g') "$@";;       # convert '-abc' to '-a' '-b' '-c'
+            --)         while [ "$1" != "$EOL" ]; do set -- "$@" "$1"; shift; done;;  # process remaining arguments as positional
+            -*)         usage_error "unknown option: '$opt'";;                        # catch misspelled options
+            *)          usage_error "this should NEVER happen ($opt)";;               # sanity test for previous patterns
+    done
+fi
+
+GLOBS=("$@")
+declare -r BACKUP="${PROJDIR}/backup"
+
+set -x
+
+cd "$PROJDIR"
+
+mkdir -p "$OUTDIR" "$BACKUP"
+
+# Rewrite the files with escodegen so they can be more easily compared to the mutated files
+for g in "${GLOBS[@]}"; do
+    find -maxdepth 0 -name "$g" -type f -exec node /bakerx/support/index.js mutate -f none -o "{}" "{}" \;
+    cp "$g" "$BACKUP"
+done
+
+run_step
+
+for (( i=0; i<=$ITERATIONS; i++ )); do
+    node /bakerx/support/index.js mutate -o "${PROJDIR}" "${GLOBS[@]}"
+    run_step
+
+    # record code-diffs for this iteration
+    for g in "${GLOBS[@]}"; do
+        find -maxdepth 0 -name "${g}" -type f -exec diff "{}" "$BACKUP/{}" >> "$OUTPUT/iteration_${i}.diff" \;
+        # Restore the originals
+        cp "$BACKUP/${g}" "$PROJDIR"
+    done
+done
+
+rm -r "$BACKUP"
+
+coverage_report | tee -a "$OUTDIR/coverage_report"
