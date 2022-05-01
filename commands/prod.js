9365448e3754d3bf23bee5166ea02e72736e94f8
Edwin Sarver <edwin.l.sarver@gmail.com>
2022-05-01 15:39:54 -0400

    extra changes to make adding a local-deployment method easier

--- commands/prod.js
+++ commands/prod.js
@@ -6,19 +6,51 @@ const mustache = require('mustache');
 const Env = process.env;
 
 
-exports.command = 'prod [direction]';
+//TODO add build file to get provider type.
+exports.command = 'prod [direction] [job_name] [build_file]';
 exports.desc = 'Control Cloud Environment Provisioning';
 exports.builder = yargs => {
     yargs.options({
     });
 };
 
+function azure_up(json) {
+    try {
+        await ssh(`/bakerx/lib/scripts/cloud_provision.sh ${mustache.render("{{cloud_pass}}", Env)} ${mustache.render("{{root_pass}}", Env)} ${mustache.render("{{tenent}}", Env)} ${mustache.render("{{cloud_username}}", Env)}`, json);
+
+    } catch (err) {
+        console.log(chalk.red(`Error running cloud provisioning script \n ${e}`));
+    }
+}
+
+function azure_down(json) {
+    try {
+        await ssh(`az group delete -y -n csc519-devops-rg`, json);
+
+    } catch (err) {
+        console.log(chalk.red(`Error running cloud deprovisioning script \n ${e}`));
+    }
+}
+
+function local_up(json) {
+    //TODO use bakerx to bring up some VMs, save inventory file
+}
+
+function local_down(json) {
+    //TODO use bakerx to tear down some VMs using implicit "inventory" file, remove inventory file.
+
+}
+
 exports.handler = async argv => {
 
-    let { direction } = argv;
+    let { direction, job_name, build_file } = argv;
 
     var obj = cp.execSync("bakerx ssh-info m1 --format json");
     var json = JSON.parse(obj);
+    let factory = new BuildFactory(fs.readFileSync(build_file, 'utf8'))
+    factory.parse();
+    let job = factory.jobs.get(job_name);
+    const provider = job.deploy.provider;
 
     if (direction == "up") {
         
@@ -28,24 +60,38 @@ exports.handler = async argv => {
 
         console.log(chalk.green("Provisioning Cloud Servers..."));
 
-        if (!Env.hasOwnProperty("cloud_pass")) {
-            throw new Error(`Must have a "cloud_pass" key=value pair in the .env file`);
-        }
-        try {
-            await ssh(`/bakerx/lib/scripts/cloud_provision.sh ${mustache.render("{{cloud_pass}}", Env)} ${mustache.render("{{root_pass}}", Env)} ${mustache.render("{{tenent}}", Env)} ${mustache.render("{{cloud_username}}", Env)}`, json);
+        //TODO add parsing of build.yml file to get provider type.
+        switch (provider) {
+            case 'azure':
+                if (!Env.hasOwnProperty("cloud_pass")) {
+                    throw new Error(`Must have a "cloud_pass" key=value pair in the .env file`);
+                }
+                azure_up(json);
+                break;
+            case 'local':
+                local_up(json);
+                break;
+            default:
+                throw new TypeError(`Unsupported provider "${provider}"`);
 
-        } catch (err) {
-            console.log(chalk.red(`Error running cloud provisioning script \n ${e}`));
         }
     } else if (direction == "down") {
         ///////////////////////////////////////////////////////////////////////////
         //////////////////// DEPROVISION CLOUD RESOURCES //////////////////////////
         ///////////////////////////////////////////////////////////////////////////
-        try {
-          await ssh(`az group delete -y -n csc519-devops-rg`, json);
-    
-        } catch (err) {
-            console.log(chalk.red(`Error running cloud deprovisioning script \n ${e}`));
+        switch (provider) {
+            case 'azure':
+                if (!Env.hasOwnProperty("cloud_pass")) {
+                    throw new Error(`Must have a "cloud_pass" key=value pair in the .env file`);
+                }
+                azure_down(json);
+                break;
+            case 'local':
+                local_down(json);
+                break;
+            default:
+                throw new TypeError(`Unsupported provider "${provider}"`);
+
         }
     } else {
         console.log(chalk.red("Usage 'prod up' or 'prod down'"));
